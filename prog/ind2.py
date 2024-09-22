71ca19393ec66c15310aac59cab9b4c15810687b
BEBEBRRREM <XeniaLuxury@yandex.ru>
2024-09-22 13:15:07 +0300

    22

---
+++ prog/ind2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Triangle:
+    def __init__(self, a=1, b=1, c=1, ab=1, ac=1, bc=1):
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        ac = int(ac)
+        ab = int(ab)
+        bc = int(bc)
+
+
+    def 
